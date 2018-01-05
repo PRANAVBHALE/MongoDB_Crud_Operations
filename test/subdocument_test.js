46a77b932357f6a5d60f765f1ad99e4fb9d82e25
PRANAVBHALE <bhalepranav25@gmail.com>
2018-01-05 20:51:44 +0530

    adding and removeing subdocument

---
+++ test/subdocument_test.js
@@ -0,0 +1,60 @@
+const assert = require('assert');
+const User = require('../src/user')
+
+describe('Subdocument',()=>{
+  it('should create a subdocument',(done)=>{
+    const joe = new User({
+      name:'Joe',
+      posts:[{
+          title:'My fav food'
+      }]
+    })
+
+    joe.save()
+      .then(()=>User.findOne({name:'Joe'}))
+      .then((user)=>{
+        assert(user.posts[0].title ==='My fav food')
+        done()
+      })
+  })
+
+
+  it('should add subdocuments to an existing record',(done)=>{
+    const joe = new User({
+      name:'Joe',
+      posts:[]
+    })
+
+    joe.save()
+      .then(()=>User.findOne({name:'Joe'}))
+      .then((user)=>{
+        user.posts.push({title:'New Post'})
+        return user.save()
+      })
+      .then(()=>User.findOne({name:"Joe"}))
+      .then((user)=>{
+        assert(user.posts[0].title === 'New Post')
+        done()
+      })
+  })
+
+
+  it('should delete subdocuments to an existing record',(done)=>{
+    const joe = new User({
+      name:'Joe',
+      posts:[{title:'My fav food'}]
+    })
+
+    joe.save()
+      .then(()=>User.findOne({name:'Joe'}))
+      .then((user)=>{
+        user.posts[0].remove()
+        return user.save()
+      })
+      .then(()=>User.findOne({name:"Joe"}))
+      .then((user)=>{
+        assert(user.posts.length === 0)
+        done()
+      })
+  })
+})
